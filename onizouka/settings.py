372592aed13ad02867a817e9120c306a64c845c4
Alassane9869 <150971756+Alassane9869@users.noreply.github.com>
2026-09-24 17:30:43 +0000

    feat: make MEDIA_ROOT configurable via environment variable

--- onizouka/settings.py
+++ onizouka/settings.py
@@ -179,7 +179,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = Path(config('MEDIA_ROOT', default=str(BASE_DIR / 'media')))
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
