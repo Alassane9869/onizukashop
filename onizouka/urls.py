f05b72b569deb3223c1a81e6e7d991424cb240bd
Alassane9869 <150971756+Alassane9869@users.noreply.github.com>
2026-09-24 16:40:49 +0000

    fix: serve media files unconditionally via re_path for cPanel/Passenger

--- onizouka/urls.py
+++ onizouka/urls.py
@@ -21,6 +21,12 @@ urlpatterns = [
     path('', include('shop.urls', namespace='shop')),
 ]
 
+from django.urls import re_path
+from django.views.static import serve
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
+
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
